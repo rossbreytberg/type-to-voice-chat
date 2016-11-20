b4a0090035faf58b1728bd5aaca413bcb69260ba
Ross <Ross@PC>
2016-11-20 01:36:58 -0800

    Add project files.

---
+++ Type-to-Voice-Chat/js/main.js
@@ -0,0 +1,194 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232509
+
+(function () {
+	"use strict";
+
+	const app = WinJS.Application;
+	let isFirstActivation = true;
+
+    // Consts
+	const MESSAGE_FILE_NAME = "message.wav";
+
+    // DOM Elements
+	let devicePickerButton = null;
+	let messageInput = null;
+
+    // Message History
+	const messageHistory = [];
+	let messageHistoryIndex = 0;
+
+    // Audio
+	const synthesizer = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
+	let outputDeviceInfo = null;
+
+	app.onactivated = function (args) {
+		if (isFirstActivation) {
+		    args.setPromise(WinJS.UI.processAll().then(function completed() {
+		        document.addEventListener("keydown", onKeyDown);
+
+		        devicePickerButton = document.getElementById(
+                    "devicePickerButton"
+                );
+		        devicePickerButton.addEventListener(
+                    "click",
+                    onDevicePickerButtonClick
+                );
+
+		        messageInput = document.getElementById("messageInput");
+		        messageInput.addEventListener("input", onMessageInputChange);
+		    }));
+		}
+		isFirstActivation = false;
+	};
+
+	function onKeyDown(eventInfo) {
+	    switch (eventInfo.keyCode) {
+	        case 13: // Enter
+	            return onMessageSubmit();
+	        case 38: // Up
+	            return prevMessage();
+	        case 40: // Down
+	            return nextMessage();
+	    }
+	}
+
+	function onMessageInputChange(eventInfo) {
+        // Update newest message in history as it's typed.
+	    messageHistory[0] = messageInput.value;
+	}
+
+	function onMessageSubmit() {
+	    const message = messageInput.value;
+	    const streamPromise = synthesizer.synthesizeTextToStreamAsync(message);
+
+	    // Get audio buffer.
+	    const bufferPromise = streamPromise.then(stream => {
+	        const dataReader = new Windows.Storage.Streams.DataReader(stream);
+	        return dataReader.loadAsync(stream.size).then(
+                () => dataReader.readBuffer(stream.size)
+            );
+	    });
+
+	    // Write buffer to temp file.
+	    // TODO: Is this necessary?
+	    bufferPromise.then(buffer => {
+	        const tempFolder =
+                Windows.Storage.ApplicationData.current.temporaryFolder;
+	        const filePromise =
+                tempFolder.tryGetItemAsync(MESSAGE_FILE_NAME).then(file => {
+                    if (file === null) {
+                        return tempFolder.createFileAsync(MESSAGE_FILE_NAME);
+                    }
+                    return file;
+                });
+	        filePromise.then(file => Windows.Storage.FileIO.writeBufferAsync(
+                file,
+                buffer
+            ));
+	    });
+
+	    // Use AudioGraph to play buffer.
+	    const audioGraphSettings = new Windows.Media.Audio.AudioGraphSettings(
+            Windows.Media.Render.AudioRenderCategory.communications
+        );
+	    audioGraphSettings.primaryRenderDevice = outputDeviceInfo;
+	    const audioGraphPromise = Windows.Media.Audio.AudioGraph.createAsync(
+            audioGraphSettings
+        ).then(createAudioGraphResult => {
+            if (createAudioGraphResult.status ===
+                Windows.Media.Audio.AudioGraphCreationStatus.success) {
+                return createAudioGraphResult.graph;
+            }
+            throw new Error('Error creating AudioGraph', createAudioGraphResult.status);
+	    });
+	    const inputNodePromise = Promise.all([
+            audioGraphPromise,
+            bufferPromise,
+            streamPromise
+	    ]).then(([
+            audioGraph,
+            buffer,
+            stream
+	    ]) => {
+            const inputNode = audioGraph.createInputNode();
+            const audioFrame = new Windows.Media.audioFrame(stream.size);
+            const audioBuffer = audioFrame.lockBuffer(
+                Windows.Media.AudioBufferAccessMode.write
+            );
+	        // TODO: Copy buffer to audioBuffer
+            inputNode.addFrame(audioFrame);
+            return inputNode;
+	    });
+	    const outputNodePromise = audioGraphPromise.then(
+            audioGraph => audioGraph.createDeviceOutputNodeAsync(
+                Windows.Media.Render.AudioRenderCategory.communications
+            )
+        );
+ 
+	    // Playback message using Blob.
+	    // TODO: Remove this after AudioGraph playback works.
+	    streamPromise.done(stream => {
+	        // Convert the stream to a URL Blob.
+	        const blob = MSApp.createBlobFromRandomAccessStream(
+                stream.ContentType,
+                stream
+            );
+	        const blobURL = URL.createObjectURL(blob, { oneTimeOnly: true });
+	        const audio = new Audio();
+	        audio.src = blobURL;
+	        audio.play();
+	    });
+
+        // Update message history.
+	    if (messageHistoryIndex > 0) {
+	        // If a message from history was sent, overwrite the
+            // newest index with that message and reset the index.
+	        messageHistory[0] = messageHistory[messageHistoryIndex];
+	        messageHistoryIndex = 0;
+	    }
+	    messageHistory.unshift("");
+	    messageInput.value = '';
+	}
+
+	function prevMessage() {
+	    if (messageHistoryIndex >= messageHistory.length - 1) {
+            // Reached end of history.
+	        return;
+	    }
+	    messageHistoryIndex++;
+	    messageInput.value = messageHistory[messageHistoryIndex];
+	}
+
+	function nextMessage() {
+	    if (messageHistoryIndex === 0) {
+            // Reached beginning of history.
+	        return;
+	    }
+	    messageHistoryIndex--;
+	    messageInput.value = messageHistory[messageHistoryIndex];
+	}
+
+	function onDevicePickerButtonClick() {
+	    devicePickerButton.disabled = true;
+
+	    const buttonRect = devicePickerButton.getBoundingClientRect();
+	    const devicePicker = new Windows.Devices.Enumeration.DevicePicker();
+        // TODO: Filter only audio devices.
+	    devicePicker.filter().pickSingleDeviceAsync({
+	        x: buttonRect.left,
+	        y: buttonRect.top,
+	        width: buttonRect.width,
+	        height: buttonRect.height,
+	    }).done(deviceInfo => {
+	        if (deviceInfo !== null) {
+	            outputDeviceInfo = deviceInfo;
+	            console.log(deviceInfo);
+	        }
+	        devicePickerButton.disabled = false;
+	    });
+	}
+
+	app.start();
+
+})();
